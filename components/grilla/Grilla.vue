f79f2bbe77900a3c6aec525c07a585606827fbb6
DaBango <guerreroeezequiel@gmail.com>
2024-06-04 15:51:28 -0300

    boton de edicion-cancelar | aceptar

--- components/grilla/Grilla.vue
+++ components/grilla/Grilla.vue
@@ -11,12 +11,12 @@
                 <table class="table-auto">
                     <thead>
                         <tr>
-                            <th v-for="(value, key) in consulta[0]" :key="key" class="px-4 py-2">{{ key }}</th>
+                            <th v-for=" (value, key) in consulta[0]" :key="key" class="px-4 py-2">{{ key }}</th>
                         </tr>
                     </thead>
-                    <tbody>
+                    <tbody :class="{ ' border-green-500': isEditing }">
                         <tr v-for="(item, index) in consulta" :key="index">
-                            <td v-for="(value, key) in item" :key="key" class="border-spacing-0 outline-2"
+                            <td v-for="(value, key) in item" :key="key" class="border-spacing-0 outline-2 "
                                 :class="{ 'border-cyan-600': isEditing && isFieldEditable(key), 'bg-slate-100': isFieldEditable(key), 'bg-slate-300': !isFieldEditable(key) }">
                                 <input v-model="item[key]" :readonly="!isEditing || !isFieldEditable(key)"
                                     class="bg-transparent focus:outline-none focus:border-blue-500 border px-4 py-2 rounded-sm">
@@ -26,8 +26,11 @@
                 </table>
             </div>
         </div>
-        <button class="mt-7 bg-cyan-600 w-20 h-7" @click="toggleEdit">
-            {{ isEditing ? 'Guardar' : 'Editar' }}
+        <button class="mt-7 bg-cyan-600 w-20 h-7" @click="toggleEdit()">
+            {{ isEditing ? 'Cancelar' : 'Editar' }}
+        </button>
+        <button class="mt-7 bg-cyan-600 w-20 h-7" @click="saveChanges()">
+            Guardar
         </button>
     </div>
 </template>
@@ -48,7 +51,8 @@ export default {
         let modelSchema = ref(null);
         let editableFields = ref({});  // Nuevo estado
         let isEditing = ref(false);  // Nuevo estado
-
+        let changedItems = ref({});
+        let originalConsulta = ref(null);  // Nuevo estado
 
         onMounted(async () => {
             const response = await fetch(props.url);
@@ -73,25 +77,61 @@ export default {
             }
         });
 
-        const toggleEdit = () => {
-            isEditing.value = !isEditing.value;
-        };
-
+        // chequea editables
         const isFieldEditable = (key) => {
             const result = modelSchema.value?.find(field => field.Field === key)?.editable;
             return Boolean(result);
+        };
+
+        const toggleEdit = () => {
+            if (!isEditing.value) {
+                originalConsulta.value = JSON.parse(JSON.stringify(consulta.value));
+            }
+            isEditing.value = !isEditing.value;
+        };
+
+        const saveChanges = async () => {
+            consulta.value.forEach((item, index) => {
+                if (!originalConsulta.value[index]) {
+                    return;
+                }
+
+                Object.keys(item).forEach(key => {
+                    if (JSON.stringify(item[key]) !== JSON.stringify(originalConsulta.value[index][key])) {
+                        changedItems.value[item.id] = item;
+                    }
+                });
+            });
+
+            for (let id in changedItems.value) {
+                const item = changedItems.value[id];
+                const response = await fetch(`http://localhost:3333/articulos/${id}`, {
+                    method: 'PUT', // Aquí es donde especificas el método PUT
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(item)
+                });
+
+                if (!response.ok) {
+                    console.error('Error al guardar los cambios: ', response.status);
+                }
+            }
+
+            changedItems.value = {};
+            originalConsulta.value = null;  // Limpiar la copia original de consulta
+            toggleEdit();
         };
 
         return {
             consulta,
             open,
             editableFields,
-            toggleEdit,  // Cambiado de toggleEditable a toggleEdit
-            isEditing,  // Asegúrate de devolver isEditing aquí
-            isFieldEditable
+            toggleEdit,
+            isEditing,
+            isFieldEditable,
+            saveChanges
         };
     }
-
 };
-
 </script>
